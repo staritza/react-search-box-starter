2e592d1f3d0825309726dde4cb3ffd0400d5a73b
Ama <bbchica@gmail.com>
2018-01-24 23:16:29 -0600

    Introduce react

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
-document.getElementById('root').innerHTML = "<div class='search-bar'><input type='text' placeholder='Search..'/></div>";
+//document.getElementById('root').innerHTML = "<div class='search-bar'><input type='text' placeholder='Search..'/></div>";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+
+//React.createElement(component, props, ...children)
+
+class SearchComponent extends React.Component {
+  render() {
+    return React.createElement(
+        'div',
+        { 'class': 'search-bar' },
+        React.createElement('input', { type: 'text', placeholder: 'Search..' })
+      );
+  }
+}
+
+ReactDOM.render(React.createElement(SearchComponent, null, null), document.getElementById('root'));
